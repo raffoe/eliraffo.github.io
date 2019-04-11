d2e06135d19208d38259b6c27dada545bec89e12
eliraffo <48398467+eliraffo@users.noreply.github.com>
2019-04-10 22:25:32 -0700

    Delete SQL_AccersoryPriceByConsole.sql

--- OBA444/SQL_AccersoryPriceByConsole.sql
+++
@@ -1,6 +0,0 @@
-SELECT pc.ProductAttribute, pt.ConsoleCode, round(avg(ht.NewPrice),2) AvgPrice
-FROM historytable ht, producttable pt, productcode pc
-WHERE ht.ASIN = pt.ASIN and pc.ProductCode = pt.ProductCode and
-(pc.ProductCode = 'AC_CON' or pc.ProductCode = 'AC_CSL+')
-GROUP BY pc.ProductAttribute, pt.ConsoleCode
-order by pc.ProductAttribute Desc, AvgPrice Asc
