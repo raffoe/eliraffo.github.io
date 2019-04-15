9a2e8e2252c952090abbecae2fe55e55203e6797
eliraffo <48398467+eliraffo@users.noreply.github.com>
2019-04-15 16:23:19 -0700

    Delete thing_insert.sql

--- cit381-assign01/thing_insert.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `test`.`assignment_1_cit381` (`Manufacturer`, `Shape`, `Kilo_Newton`, `Color`, `Price`, `ID`) VALUES ('Black Diamond', 'D', '24', 'Orange', '13.95', '1');
-INSERT INTO `test`.`assignment_1_cit381` (`Manufacturer`, `Shape`, `Kilo_Newton`, `Color`, `Price`, `ID`) VALUES ('Petzl', 'P', '27', 'Silver', '17.95', '2');
-INSERT INTO `test`.`assignment_1_cit381` (`Manufacturer`, `Shape`, `Kilo_Newton`, `Color`, `Price`, `ID`) VALUES ('Metolius', 'P', '24', 'Green', '9.95', '3');
-INSERT INTO `test`.`assignment_1_cit381` (`Manufacturer`, `Shape`, `Kilo_Newton`, `Color`, `Price`, `ID`) VALUES ('CAMP', 'D', '22', 'Black', '11.95', '4');
-INSERT INTO `test`.`assignment_1_cit381` (`Manufacturer`, `Shape`, `Kilo_Newton`, `Color`, `Price`, `ID`) VALUES ('Mammut', 'P', '24', 'Blue', '17.95', '5');
